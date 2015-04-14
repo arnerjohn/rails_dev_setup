c8325834285ae6eefc76134f9c64c1c804c8a346
maxdignan <maxdignan@gmail.com>
2015-04-14 16:05:25 -0400

    authorization added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,19 +13,14 @@ class ApplicationController < ActionController::Base
       render json: {message: 'Invalid API Token'}, status: 401
       return
     end
-    puts @api_key
     @rider = Rider.find(@api_key) if @api_key
   end
 
   def restrict_access_by_header
     return @api_key if @api_key
-    puts 'hello'
     authenticate_with_http_token do |token|
-      puts token
       token.slice! 'token:'
-      puts token
       @api_key = ApiKey.where({ access_token: (token) }).first
-      puts @api_key
       return @api_key
     end
   end
