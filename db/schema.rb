23a4f75c3f45f8380bf96ed0f8f5699e2886c580
maxdignan <maxdignan@gmail.com>
2015-04-13 23:31:42 -0400

    fixing merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20150412203345) do
 
   create_table "api_keys", force: true do |t|
